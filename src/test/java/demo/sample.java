15a0ce2290a3ea5c67ec8758ebd6d475022f39f3
saini <saini123@gmail.com>
2022-10-18 12:51:38 +0530

    parameter added in maven

--- src/test/java/demo/sample.java
+++ src/test/java/demo/sample.java
@@ -2,16 +2,28 @@ package demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class sample {
-
+	
+	@Parameters("Browser")
 	@Test
-	public void startBrowser() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+	public void startBrowser(String browserName) {
+		WebDriver driver=null;
+		System.out.println("Parameter browser name is :"+browserName);
+		if(browserName.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();
+		}
+		else if(browserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			 driver=new EdgeDriver();
+		}
+	
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.quit();
