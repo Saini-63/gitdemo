3e04879e16b05406c5f608837d5cc88f1da026d1
saini <saini123@gmail.com>
2022-10-18 11:20:24 +0530

    All file commit in first attempt

---
+++ src/test/java/demo/sample.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class sample {
+
+	@Test
+	public void startBrowser() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.quit();
+	}
+
+}
